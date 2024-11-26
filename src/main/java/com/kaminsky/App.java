5a9c060133c24c8cb2509d03ffe7c738dddd0d15
Philip Kaminsky <kaminsky8836@gmail.com>
2024-11-26 15:50:46 +0300

    final commit

--- src/main/java/com/kaminsky/App.java
+++ src/main/java/com/kaminsky/App.java
@@ -1,9 +1,25 @@
 package com.kaminsky;
 
-public class App 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class App
 {
     public static void main( String[] args )
     {
+        App app = new App();
+        String[] test = {"one", "two", "three", "one"};
+        Map<String, Integer> testMap = app.convertArrayToMap(test);
+        testMap.forEach((key, value) ->
+                System.out.println(key + ":" + value));
+    }
 
+    public <T> Map<T, Integer> convertArrayToMap(T[] arr) {
+        Map<T, Integer> resultMap = new HashMap<T, Integer>();
+
+        return Arrays.stream(arr)
+                .collect(Collectors.toMap(key -> key, value -> 1, Integer::sum));
     }
 }
